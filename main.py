9199bb7377e710fdc5869a0cda2091042da145a6
Rogozhin-Dmitry <62459927+Rogozhin-Dmitry@users.noreply.github.com>
2021-04-10 22:05:05 +0300

    о чудо, наконец я подключил главниую страницу

--- main.py
+++ main.py
@@ -79,12 +79,49 @@ def main():
     db_sess.commit()
 
 
-@app.route("/")
+@app.route('/')
+@app.route('/index')
 def index():
     session['current_second_test_length'] = 2
     session['current_first_test_length'] = 2
     session['current_images_stack'] = []
-    return render_template("index.html")
+    return render_template('index.html')
+
+
+@app.route('/js/plugins/waypoint/jquery.waypoints.min.js')
+def url_1():
+    url = url_for('static', filename='/js/plugins/waypoint/jquery.waypoints.min.js')[1:].replace('//', '/')
+    return open(url.replace('/', '\\')).read()
+
+
+@app.route('/js/plugins/swiper/js/swiper.min.js')
+def url_2():
+    url = url_for('static', filename='/js/plugins/swiper/js/swiper.min.js')[1:].replace('//', '/')
+    return open(url.replace('/', '\\')).read()
+
+
+@app.route('/js/plugins/ajaxchimp/jquery.ajaxchimp.min.js')
+def url_3():
+    url = url_for('static', filename='/js/plugins/ajaxchimp/jquery.ajaxchimp.min.js')[1:].replace('//', '/')
+    return open(url.replace('/', '\\')).read()
+
+
+@app.route('/js/plugins/matchHeight/jquery.matchHeight.min.js')
+def url_4():
+    url = url_for('static', filename='/js/plugins/matchHeight/jquery.matchHeight.min.js')[1:].replace('//', '/')
+    return open(url.replace('/', '\\')).read()
+
+
+@app.route('/js/plugins/waypoint/sticky.min.js')
+def url_5():
+    url = url_for('static', filename='/js/plugins/waypoint/sticky.min.js')[1:].replace('//', '/')
+    return open(url.replace('/', '\\')).read()
+
+
+@app.route('/js/plugins/scrollTo/jquery.scrollTo.min.js')
+def url_6():
+    url = url_for('static', filename='/js/plugins/scrollTo/jquery.scrollTo.min.js')[1:].replace('//', '/')
+    return open(url.replace('/', '\\')).read()
 
 
 @app.route("/tests_list", methods=['GET', 'POST'])
